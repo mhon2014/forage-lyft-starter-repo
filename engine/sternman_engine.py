8c0558f8c5bbf133dd04d546e0f7f4ee1c71811a
Michael James Hon <mhon2014@my.fit.edu>
2023-01-21 00:54:35 -0500

    added battery classes and year thresholds for battery service

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,7 +1,7 @@
 from engine import Engine
 
 class SternmanEngine(Engine):
-    def __init__(self, warning_light_is_on):
+    def __init__(self, warning_light_is_on: bool):
         self.warning_light_is_on = warning_light_is_on
 
     def needs_service(self) -> bool:
--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -2,7 +2,7 @@
 from engine import Engine
 
 class WilloughbyEngine(Engine):
-    def __init__(self, current_mileage, last_service_mileage):
+    def __init__(self, current_mileage: int, last_service_mileage: int):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
