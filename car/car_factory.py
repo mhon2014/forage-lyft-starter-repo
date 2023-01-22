2cfb3387bba7945f970a94bda27a85a0159923db
Michael James Hon <mhon2014@my.fit.edu>
2023-01-22 10:46:51 -0500

    forgot to add car_factory update

--- car/car_factory.py
+++ car/car_factory.py
@@ -11,26 +11,41 @@ from engine.engine import *
 
 class CarFactory(ABC):
     def create_calliope(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
-        engine = CapuletEngine()
-        battery = SpindlerBattery()
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
         calliope = Car(engine,battery)
 
         return calliope
 
 
     def create_glissade(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
-        pass
+        
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        glissade = Car(engine,battery)
+
+        return glissade
 
 
     def create_palindrome(current_date: datetime, last_service_date: datetime, warning_light_on: bool) -> Car:
-        pass
+        engine = SternmanEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        palindrome = Car(engine,battery)
+
+        return palindrome
 
 
     def create_rorschach(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
-        pass
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
+        roschach = Car(engine,battery)
+
+        return roschach
 
 
     def create_thovex(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
-        pass
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
+        thovex = Car(engine,battery)
 
-    
+        return thovex
