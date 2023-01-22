6b4949f5420674cbb399da9b42e12f0368b997ce
Michael James Hon <mhon2014@my.fit.edu>
2023-01-22 10:46:13 -0500

    added serviceable interface for car and finished car_factory

--- battery/battery.py
+++ battery/battery.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from datetime import datetime
+
 '''
 Interface for battery class
 '''
--- car/car.py
+++ car/car.py
@@ -5,12 +5,16 @@ from battery.battery import Battery
 from engine.engine import Engine
 
 
-class Car:
+class Serviceable(ABC):
+    @abstractmethod
+    def needs_service(self) -> bool:
+        pass
+
+class Car(Serviceable):
     def __init__(self, Engine: Engine, Battery: Battery):
         self.engine = Engine
         self.battery = Battery
 
-    @abstractmethod
     def needs_service(self):
         return self.engine.needs_service() \
                 and self.battery.needs_service()
